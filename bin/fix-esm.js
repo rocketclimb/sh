64cff7229362293041d5a641ac06894e9c6b7d79
Jeferson Amorim <amorimjj@gmail.com>
2024-05-04 19:54:59 -0300

    fix esm fixer skip replace if file does not exists

--- bin/fix-esm.js
+++ bin/fix-esm.js
@@ -1,23 +1,33 @@
 import fs from "node:fs";
-import { join } from "node:path";
+import { join, basename } from "node:path";
 import shell from "shelljs";
 import { EXIT_CODES } from "./config.js";
 
-const fixEsmFile = (file) => {
+const fixEsmFile = (validFiles, file) => {
   const content = fs.readFileSync(file, { encoding: "utf8" });
-  fs.writeFileSync(
-    file,
-    content.replace(/([import|export].*from "\.\/[\w|\/|-]+)";/g, '$1.mjs";'),
-    { encoding: "utf8" }
+  const updatedContent = content.replace(
+    /([import|export].*from "\.\/[\w|\/|-]+)";/g,
+    (match, group) => {
+      let [statement, file] = group.split("./");
+      return validFiles.includes(`${file.split("/").pop()}.mjs`)
+        ? `${statement}./${file}.mjs";`
+        : match;
+    }
   );
+  fs.writeFileSync(file, updatedContent, { encoding: "utf8" });
 };
 
 export const fixEsm = (args) => {
   const pwd = shell.pwd().toString();
-  args.forEach((inputName) => {
-    shell
-      .find(inputName)
-      .forEach((arg) => /.*\.mjs$/i.exec(arg) && fixEsmFile(join(pwd, arg)));
-  });
+
+  const files = [].concat(
+    ...args.map((inputName) =>
+      shell.find(inputName).filter((file) => /.*\.mjs$/i.exec(file))
+    )
+  );
+
+  const validFiles = files.map((file) => basename(file));
+
+  files.forEach((file) => fixEsmFile(validFiles, join(pwd, file)));
   return EXIT_CODES.SUCCESS;
 };
